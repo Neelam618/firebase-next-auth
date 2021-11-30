673a4296526183ddcf7316eb532e3b6961274304
Neelam Chavan <neelamchavan410@gmail.com>
2021-11-30 16:59:52 +0530

    added sign in in navbar component

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import { useRouter } from 'next/router'
 
 const nfaDependencyVersion =
   require('../package.json').dependencies['next-firebase-auth']
@@ -8,27 +13,49 @@ const firebaseDependencyVersion =
   require('../package.json').dependencies.firebase
 
 const Header = ({ email, signOut }) => {
+  const router = useRouter()
+
   return (
     <div>
 
-      {email ? (
-        <button
-          type="button"
-          onClick={() => {
-            signOut()
-          }}
-        >
-          Sign out
-        </button>
-      ) : (
-        <Link href="/auth">
-          <a>
-            <button type="button">
-              Sign in
-            </button>
-          </a>
-        </Link>
-      )}
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+        <Container>
+          <Navbar.Brand href="#home">innerKore Platform</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/how-to-build" className={router.pathname == "/how-to-build" ? 'active' : null}>How to build</Nav.Link>
+              <Nav.Link href="/partner-program" className={router.pathname == "/partner-program" ? 'active' : null}>Partner program</Nav.Link>
+              <Nav.Link href="/docs" className={router.pathname == "/docs" ? 'active' : null}>Documentation</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="/" className={router.pathname == "/" ? 'active' : null}>My Integrations</Nav.Link>
+              <Nav.Link href="/templates" className={router.pathname == "/templates" ? 'active' : null}>Templates</Nav.Link>
+              <Nav.Link>
+                {email ? (
+                  <NavDropdown title={email} id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Zapier.com</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Settings</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4" onClick={() => {
+                      signOut()
+                    }}>Sign out</NavDropdown.Item>
+                  </NavDropdown>
+
+                ) : (
+                  <Link href="/auth">
+                    <a>
+                      <button type="button">
+                        Sign in
+                      </button>
+                    </a>
+                  </Link>
+                )}
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import {
   withAuthUserTokenSSR,
 } from 'next-firebase-auth'
 import Header from '../components/Header'
-import DemoPageLinks from '../components/DemoPageLinks'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const styles = {
   content: {
@@ -21,20 +21,6 @@ const Demo = () => {
   return (
     <div>
       <Header email={AuthUser.email} signOut={AuthUser.signOut} />
-      {/* <div style={styles.content}>
-        <div style={styles.infoTextContainer}>
-          <h3>Home</h3>
-          <p>
-            This page does not require authentication, so it won't redirect to
-            the login page if you are not signed in.
-          </p>
-          <p>
-            If you remove `getServerSideProps` from this page, it will be static
-            and load the authed user only on the client side.
-          </p>
-        </div>
-        <DemoPageLinks />
-      </div> */}
     </div>
   )
 }
